547f12ffed0cf3a469a7b8f2d2e48983f424b1fe
jingyuan <zjingyuan@trip.com>
2025-09-03 10:17:25 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/base64-picture-viewer/' : '/',
+  base: '/base64-picture-viewer/',
   build: {
     outDir: 'dist',
   },
